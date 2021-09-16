281597a23483149a54e87d4d6ffb27280feb6961
ohgihara <schu22to0716@icloud.com>
2021-09-16 10:37:26 +0900

    ツイートの詳細表示機能の実装

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,4 +1,6 @@
 class MusiciansController < ApplicationController
+  before_action :set_musician, only: [:edit, :show]
+
   def index
     @musicians = Musician.all
   end
@@ -17,7 +19,6 @@ class MusiciansController < ApplicationController
   end
 
   def edit
-    @musician = Musician.find(params[:id])
   end
 
   def update
@@ -25,9 +26,16 @@ class MusiciansController < ApplicationController
     musician.update(musician_params)
   end
 
+  def show
+  end
+
   private
   def musician_params
     params.require(:musician).permit(:musician_name, :musician_view, :musician_song,:date_of_birth,:image)
+  end
+
+  def set_musician
+    @musician = Musician.find(params[:id])
   end
 
 end
