f9b17ebab405eb9ac0ccfbad43a6e7036518ce3f
ohgihara <schu22to0716@icloud.com>
2021-09-23 15:35:25 +0900

    ユーザーモデルの単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  validates :nickname, presence: true
+
+
   has_many :musicians
   has_many :comments
   has_many :music_terms
