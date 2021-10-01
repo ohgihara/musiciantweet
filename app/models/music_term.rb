cbb9887e95bf43ee33ab0d959960204ed18b9407
ohgihara <schu22to0716@icloud.com>
2021-10-01 17:19:07 +0900

    投稿時のエラーメッセージ、正規表現の追加

--- app/models/music_term.rb
+++ app/models/music_term.rb
@@ -1,8 +1,8 @@
 class MusicTerm < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :music_term, presence: true
-  validates :music_term_view, presence: true
+  validates :music_term, presence: { message: '「音楽用語」をご入力ください。' }
+  validates :music_term_view, presence: { message: '「用語の説明」をご入力ください。' }
 
   def self.search(search)
     if search != ""
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -3,17 +3,14 @@ class Musician < ApplicationRecord
   belongs_to_active_hash :musician_era
 
   has_one_attached :image
-  validates :image, presence:true
+  
 
-with_options presence: true do
-  validates :musician_name
-  validates :musician_view
-  validates :musician_song
-  validates :date_of_birth
-end
-
+  validates :musician_name, presence: { message: '「音楽家名」をご入力ください。' }
+  validates :image, presence: { message: '「画像」を挿入してください。' }
   validates :musician_era_id, numericality: { other_than: 1, message: "「音楽家の時代区分」をご選択ください。"}
-
+  validates :date_of_birth,format: {with: /\A[0-9]{3,4}~[0-9]{3,4}年\z/, message:"「生没年」を〇〇年(3,4桁)~〇〇年(3,4桁)でご入力ください。"}
+  validates :musician_view, presence: { message: '「音楽家の概要」をご入力ください。' }
+  validates :musician_song, presence: { message: '「代表曲」をご入力ください。' }
 
   belongs_to :user
   has_many :comments
